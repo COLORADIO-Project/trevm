bf537df43a65b6c4d4fa831b078775b7cad8bb63
Antoine Lavandier <antoine.lavandier@inria.fr>
2026-02-19 15:20:44 +0100

    feat: example featuring the sandbox
    Signed-off-by: Antoine Lavandier <antoine.lavandier@inria.fr>

---
+++ examples/sandbox-no-bindings/src/main.rs
@@ -0,0 +1,80 @@
+#![no_main]
+#![no_std]
+
+extern crate alloc;
+use alloc::string::String;
+
+use ariel_os::coap::coap_run;
+use ariel_os::debug::log::info;
+use ariel_os::debug::{ExitCode, exit};
+
+use ariel_os::time::Timer;
+use wasmtime::component::{Component, Linker, bindgen};
+use wasmtime::{Config, Engine, Store};
+
+use coap_handler::Handler;
+use coap_handler_implementations::{HandlerBuilder, ReportingHandlerBuilder, new_dispatcher};
+
+use ariel_os_bindings::wasm::coap::{
+    CanInstantiate, CoAPError, EphemeralCapsule, WasmHandler, WasmHandlerWrapped,
+};
+
+use ariel_os_bindings::wasm::ArielOSHost;
+
+use ariel_os_bindings::wasm::coap::sanbdox::Sandbox;
+bindgen!({
+    world: "example-sandbox-no-bindings",
+    path: "../../wit",
+});
+
+impl CanInstantiate<ArielOSHost> for ExampleSandboxNoBindings {
+    fn instantiate(
+        linker: &mut Linker<ArielOSHost>,
+        store: &mut Store<ArielOSHost>,
+        component: Component,
+    ) -> wasmtime::Result<Self> {
+        ExampleSandboxNoBindings::instantiate(store, &component, &linker)
+    }
+}
+
+impl EphemeralCapsule<ArielOSHost, String> for ExampleSandboxNoBindings {
+    fn run(&mut self, store: &mut Store<ArielOSHost>) -> wasmtime::Result<String> {
+        self.call_run(store)
+    }
+}
+
+#[ariel_os::task(autostart)]
+async fn main() {
+    let res = run_wasm_coap_server().await;
+    info!("{:?}", defmt::Debug2Format(&res));
+    Timer::after_millis(100).await;
+    exit(ExitCode::SUCCESS);
+}
+
+async fn run_wasm_coap_server() -> wasmtime::Result<()> {
+    let mut config = Config::default();
+
+    // Options that must conform with the precompilation step
+    config.wasm_custom_page_sizes(true);
+    config.target("pulley32").unwrap();
+
+    config.table_lazy_init(false);
+    config.memory_reservation(0);
+    config.memory_init_cow(false);
+    config.memory_may_move(false);
+
+    // Options that can be changed without changing the payload
+    config.max_wasm_stack(2048);
+    config.memory_reservation_for_growth(0);
+
+    let engine = Engine::new(&config).unwrap();
+
+    let sandbox: Sandbox<'_, ArielOSHost, String, ExampleSandboxNoBindings> = Sandbox::new(&engine);
+
+    let handler = sandbox.to_handler().with_wkc();
+
+    info!("Starting Handler");
+    coap_run(handler).await;
+    #[allow(unreachable_code)]
+    Ok(())
+}
---
+++ payloads/sandbox-no-bindings/src/lib.rs
@@ -0,0 +1,47 @@
+#![no_std]
+
+#[global_allocator]
+static ALLOCATOR: talc::Talck<talc::locking::AssumeUnlockable, talc::ClaimOnOom> = {
+    static mut MEMORY: [u8; 0x400] = [0; 0x400]; // 1KiB of memory
+    let span = talc::Span::from_array((&raw const MEMORY).cast_mut());
+    talc::Talc::new(unsafe { talc::ClaimOnOom::new(span) }).lock()
+};
+
+use wit_bindgen::generate;
+
+extern crate alloc;
+generate!({
+    world: "example-sandbox-no-bindings ",
+    path: "../../wit",
+    generate_all,
+});
+
+use alloc::format;
+use alloc::string::String;
+pub struct MyComponent;
+
+impl Guest for MyComponent {
+    fn run() -> String {
+        format!("The thirtieth fibonacci number is {}", fibonacci(30))
+    }
+}
+
+
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 { return 0; }
+    else if n == 1 { return 1; }
+    let mut f_0 = 0;
+    let mut f_1 = 1;
+    for _ in 2..=n {
+        f_1 += f_0;
+        f_0 = f_1 - f_0;
+    }
+    return f_1
+}
+
+export!(MyComponent);
+
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+    core::arch::wasm32::unreachable();
+}
