2d183353c3dade6441950bfb483930266a13b39e
Antoine Lavandier <antoine.lavandier@inria.fr>
2026-02-23 16:50:47 +0100

    chore: pin the nightly version to that of Ariel OS v0.3.0
    Signed-off-by: Antoine Lavandier <antoine.lavandier@inria.fr>

--- rebuild-all-payloads.sh
+++ rebuild-all-payloads.sh
@@ -5,16 +5,16 @@ set -e
 
 for p in ble-scanner ephemeral-no-bindings ephemeral-with-bindings gpio persistent-no-bindings persistent-with-bindings udp-bindings
 do
-    ./precompile_wasm.rs --path payloads/${p}/Cargo.toml -o ./examples/${p}/payload.cwasm --config payloads/.cargo/config.toml
+    cargo +nightly-2026-01-20 -Z script precompile_wasm.rs --path payloads/${p}/Cargo.toml -o ./examples/${p}/payload.cwasm --config payloads/.cargo/config.toml --toolchain +nightly-2026-01-20
 done
 
 # These need fuel, and are usable also with 64bit native
-./precompile_wasm.rs --path payloads/async-bindings/Cargo.toml -o ./examples/async-bindings/payload.cwasm --config payloads/.cargo/config.toml --fuel
-./precompile_wasm.rs --path payloads/async-bindings/Cargo.toml -o ./examples/async-bindings/payload.pulley64f.cwasm --config payloads/.cargo/config.toml --fuel --target pulley64
+cargo +nightly-2026-01-20 -Z script precompile_wasm.rs --path payloads/async-bindings/Cargo.toml -o ./examples/async-bindings/payload.cwasm --config payloads/.cargo/config.toml --fuel --toolchain +nightly-2026-01-20
+cargo +nightly-2026-01-20 -Z script precompile_wasm.rs --path payloads/async-bindings/Cargo.toml -o ./examples/async-bindings/payload.pulley64f.cwasm --config payloads/.cargo/config.toml --fuel --target pulley64 --toolchain +nightly-2026-01-20
 
-./precompile_wasm.rs --path payloads/simple-updates-1/Cargo.toml -o ./examples/simple-updates/payload1.cwasm --config payloads/.cargo/config.toml
-./precompile_wasm.rs --path payloads/simple-updates-2/Cargo.toml -o ./examples/simple-updates/payload2.cwasm --config payloads/.cargo/config.toml
+cargo +nightly-2026-01-20 -Z script precompile_wasm.rs --path payloads/simple-updates-1/Cargo.toml -o ./examples/simple-updates/payload1.cwasm --config payloads/.cargo/config.toml --toolchain +nightly-2026-01-20
+cargo +nightly-2026-01-20 -Z script precompile_wasm.rs --path payloads/simple-updates-2/Cargo.toml -o ./examples/simple-updates/payload2.cwasm --config payloads/.cargo/config.toml --toolchain nightly-2026-01-20
 
 cp examples/simple-updates/*.cwasm examples/insecure-updates/
 
-./precompile_wasm.rs --path payloads/sensors/Cargo.toml -o examples/fake-sensor/payload.cwasm --config payloads/.cargo/config.toml
+cargo +nightly-2026-01-20 -Z script precompile_wasm.rs --path payloads/sensors/Cargo.toml -o examples/fake-sensor/payload.cwasm --config payloads/.cargo/config.toml --toolchain +nightly-2026-01-20
